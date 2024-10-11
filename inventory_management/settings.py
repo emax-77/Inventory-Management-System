017dee17891b422a9712e568631b0f7d1646531a
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-10-11 16:21:39 +0200

    Environment variables setup on settings.py

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -10,6 +10,15 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+# env values setup / raise an error if the env variable isn't set
+import os
+from django.core.exceptions import ImproperlyConfigured
+def _require_env(name):
+    value = os.getenv(name)
+    if value is None:
+        raise ImproperlyConfigured('Required environment variable "{}" is not set.'.format(name))
+    return value
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
