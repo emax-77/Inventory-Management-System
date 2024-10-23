9f0104ef4a90ef60c5c9de0daefa343bce4c3c7e
Peter Wirth <57559346+emax-77@users.noreply.github.com>
2024-10-23 09:32:23 +0200

    Create InvoiceForm on form.py

--- ims/forms.py
+++ ims/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Product, Sale
+from .models import Product, Sale, Invoice
 
 class ProductForm(forms.ModelForm):
     class Meta:
@@ -10,3 +10,12 @@ class SaleForm(forms.ModelForm):
     class Meta:
         model = Sale
         fields = ['product', 'quantity_sold']
+
+class InvoiceForm(forms.ModelForm):
+    sales = forms.ModelMultipleChoiceField(
+        queryset=Sale.objects.all(),  # Query all sales to choose from
+        widget=forms.CheckboxSelectMultiple,  # Use checkboxes for multiple selection
+    )
+    class Meta:
+        model = Invoice
+        fields = ['invoice_number', 'sales']
